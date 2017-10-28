2a6d99811571360f3f9c038e1f1642bd5eae346c
HoangThiKimPhung <hoangkimphung070994@gmail.com>
2017-10-28 21:48:28 +0700

    Ú cập nhật phần Thuê phòng và Quản trị viên

--- QuanLyKhachSan_Visual/JS/include-html.js
+++ QuanLyKhachSan_Visual/JS/include-html.js
@@ -1,15 +1,12 @@
 $(document).ready(function () {    
     //Sự kiện add header, menu-left và footer vào page
-    $.get('header.html', function (res) {
+    $.get('header-NV.html', function (res) {
         $('#header').html(res);
     });
-    $.get('menu.html', function (res) {
+    $.get('menu-NV.html', function (res) {
         $('#menu').html(res); 
         
         //Sự kiện khi click vào các sub-menu
-        $("li#submenu2").click(function () {
-            $("ul#sub-menu2").toggle();
-        });
         $("li#submenu3").click(function () {
             $("ul#sub-menu3").toggle();
         });
@@ -27,6 +24,9 @@ $(document).ready(function () {
         });
         $("li#submenu8").click(function () {
             $("ul#sub-menu8").toggle();
+        });
+        $("li#submenu9").click(function () {
+            $("ul#sub-menu9").toggle();
 
         });
 
@@ -40,27 +40,53 @@ $(document).ready(function () {
             $("li#submenu1").css("background-color", "#0d875c");
             $("li#submenu1 a:first").attr("style", "color: white !important");
         }
+        if (path.lastIndexOf('tim-phong.html') >= 0) {
+            namepage = 'TÌM PHÒNG';
+            namefile = 'tim-phong.html';
+            $("li#submenu2").css("background-color", "#0d875c");
+            $("li#submenu2 a:first").attr("style", "color: white !important");
+        }
         else if (path.lastIndexOf('tra-cuu-khach-dat.html') >= 0) {
             namepage = 'TRA CỨU > TRA CỨU KHÁCH ĐẶT';
             namefile = 'tra-cuu-khach-dat.html';
-            $("li#submenu2").css("background-color", "#0d875c");
-            $("li#submenu2 a:first").attr("style", "color: white !important");
+            $("li#submenu3").css("background-color", "#0d875c");
+            $("li#submenu3 a:first").attr("style", "color: white !important");
+        }
+        else if (path.lastIndexOf('tra-cuu-khach-hang.html') >= 0) {
+            namepage = 'TRA CỨU > TRA CỨU KHÁCH HÀNG';
+            namefile = 'tra-cuu-khach-hang.html';
+            $("li#submenu3").css("background-color", "#0d875c");
+            $("li#submenu3 a:first").attr("style", "color: white !important");
         }
         else if (path.lastIndexOf('tra-cuu-phong.html') >= 0) {
             namepage = 'TRA CỨU > TRA CỨU PHÒNG';
             namefile = 'tra-cuu-phong.html';
-            $("li#submenu2").css("background-color", "#0d875c");
-            $("li#submenu2 a:first").attr("style", "color: white !important");
+            $("li#submenu3").css("background-color", "#0d875c");
+            $("li#submenu3 a:first").attr("style", "color: white !important");
         }
-        else if (path.lastIndexOf('tim-phong.html') >= 0) {
-            namepage = 'TÌM PHÒNG';
-            namefile = 'tim-phong.html';
+        else if (path.lastIndexOf('tra-cuu-dich-vu.html') >= 0) {
+            namepage = 'TRA CỨU > TRA CỨU DỊCH VỤ';
+            namefile = 'tra-cuu-dich-vu.html';
+            $("li#submenu3").css("background-color", "#0d875c");
+            $("li#submenu3 a:first").attr("style", "color: white !important");
+        }
+        else if (path.lastIndexOf('tra-cuu-dich-vu-KH-da-su-dung.html') >= 0) {
+            namepage = 'TRA CỨU > TRA CỨU DỊCH VỤ KHÁCH HÀNG ĐÃ SỬ DỤNG';
+            namefile = 'tra-cuu-dich-vu-KH-da-su-dung.html';
+            $("li#submenu3").css("background-color", "#0d875c");
+            $("li#submenu3 a:first").attr("style", "color: white !important");
         }
         else if (path.lastIndexOf('them-don-thue-phong.html') >= 0) {
             namepage = 'QUẢN LÝ THUÊ PHÒNG > THÊM ĐƠN THUÊ PHÒNG';
             namefile = 'them-don-thue-phong.html';
-            $("li#submenu4").css("background-color", "#0d875c");
-            $("li#submenu4 a:first").attr("style", "color: white !important");
+            $("li#submenu5").css("background-color", "#0d875c");
+            $("li#submenu5 a:first").attr("style", "color: white !important");
+        }
+        else if (path.lastIndexOf('chinh-sua-don-thue-phong.html') >= 0) {
+            namepage = 'QUẢN LÝ THUÊ PHÒNG > CHỈNH SỬA ĐƠN THUÊ PHÒNG';
+            namefile = 'chinh-sua-don-thue-phong.html';
+            $("li#submenu5").css("background-color", "#0d875c");
+            $("li#submenu5 a:first").attr("style", "color: white !important");
         }
 
         $('#page-trich-dan').attr("href", namefile);
