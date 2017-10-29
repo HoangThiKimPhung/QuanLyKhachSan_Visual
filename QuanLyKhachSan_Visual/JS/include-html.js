d2bf9f52a933bf4635a9d81ccfaad6a5b1613c6c
HoangThiKimPhung <hoangkimphung070994@gmail.com>
2017-10-29 18:39:13 +0700

    Phụng cập nhật phần thuê phòng

--- QuanLyKhachSan_Visual/JS/include-html.js
+++ QuanLyKhachSan_Visual/JS/include-html.js
@@ -88,6 +88,18 @@ $(document).ready(function () {
             $("li#submenu5").css("background-color", "#0d875c");
             $("li#submenu5 a:first").attr("style", "color: white !important");
         }
+        else if (path.lastIndexOf('thong-ke-don-thue-phong.html') >= 0) {
+            namepage = 'QUẢN LÝ THUÊ PHÒNG > THỐNG KÊ ĐƠN THUÊ PHÒNG';
+            namefile = 'thong-ke-don-thue-phong.html';
+            $("li#submenu5").css("background-color", "#0d875c");
+            $("li#submenu5 a:first").attr("style", "color: white !important");
+        }
+        else if (path.lastIndexOf('chi-tiet-thong-ke-thue-phong.html') >= 0) {
+            namepage = 'QUẢN LÝ THUÊ PHÒNG > THỐNG KÊ ĐƠN THUÊ PHÒNG > CHI TIẾT';
+            namefile = 'chi-tiet-thong-ke-thue-phong.html';
+            $("li#submenu5").css("background-color", "#0d875c");
+            $("li#submenu5 a:first").attr("style", "color: white !important");
+        }
 
         $('#page-trich-dan').attr("href", namefile);
         $('#page-trich-dan').text(namepage);
